5ee2a79798f1c6a365ccafcd45efa6e1ee63a6f2
dam <dam@10.100.100.19>
2020-12-21 09:24:04 +0100

    Tercera version. Fusion clases

--- EjercicioGit/src/ejerciciogit/NuevaClase.java
+++ EjercicioGit/src/ejerciciogit/NuevaClase.java
@@ -13,5 +13,6 @@ public class NuevaClase {
 
     public static void main(String[] args) {
         System.out.println("En la rama.Se crea la clase nueva");
+        System.out.println("Fusion ambas clases en una");
     }
 }
